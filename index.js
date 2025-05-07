41d5acb51bd67318579ee993a46a69bd8bc5435f
Thanh Nguyen <52744459+thanhnv97@users.noreply.github.com>
2025-05-07 16:15:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
